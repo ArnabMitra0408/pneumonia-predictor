94037f3f8b5ad7ecd7de05ceae1dd8600784ccaf
Jacob Kuriakose <jacobjk03@gmail.com>
2024-08-23 15:43:36 -0700

    Created Data upload code

---
+++ src/raw_data_upload.py
@@ -0,0 +1,46 @@
+import os
+from azure.storage.blob import BlobServiceClient
+from argparse import ArgumentParser
+from utils.common_utils import unzip_file,read_params
+
+def data_upload(container_client, local_folder,raw_data,raw_zip_data):
+
+    unzip_file(raw_zip_data,raw_data)
+
+    # Create container if it does not exist else leave
+    try:
+        container_client.create_container()
+    except Exception as e:
+        print(f"Container already exists: {e}")
+        # Empty the container to remove the existing data
+        # overwrite = True is done 
+
+    for root, dirs, files in os.walk(local_folder):
+        for file in files:
+            local_file_path = os.path.join(root, file)
+            relative_path = os.path.relpath(local_file_path, local_folder)
+            blob_path = relative_path.replace("\\", "/")
+
+            print(f"Uploading {local_file_path} to {blob_path}...")
+
+            blob_client = container_client.get_blob_client(blob_path)
+            with open(local_file_path, "rb") as data:
+                blob_client.upload_blob(data, overwrite=True)
+
+if __name__ == '__main__':
+    # for yaml file 
+    args = ArgumentParser()
+    args.add_argument("--config_path", "-c", default='params.yaml') 
+    parsed_args = args.parse_args() # read the values of parameter/arguments passed in above line
+    configs = read_params(parsed_args.config_path) # read yaml file
+
+    connect_str = configs['container']['connect_str']
+    container_name = configs['container']['container_name']
+    local_folder = configs['data_dir']['artifacts']
+    raw_zip_data=configs['data_dir']['raw_zip_data']
+    raw_data=configs['data_dir']['raw_data']
+
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)  # Connection with storage account
+    container_client = blob_service_client.get_container_client(container_name)  # Connection with container
+    
+    data_upload(container_client, local_folder,raw_data,raw_zip_data)
---
+++ src/utils/common_utils.py
@@ -0,0 +1,47 @@
+import yaml
+import os
+import zipfile
+
+def clean_dir(dir_path:str)-> None:
+    #Removes all the files in a directory. 
+    if len(os.listdir(dir_path)) == 0:
+        print(f'directory {dir_path} already empty')
+    else:
+        for root, dirs, files in os.walk(dir_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                os.remove(file_path)
+        print(f'directory {dir_path} cleaned')
+
+def create_dir(path:str) -> None:
+    if os.path.exists(path):
+        print(f'directory {path} already exists')
+    else:
+        os.mkdir(path)
+        print(f'directory {path} created')
+
+def unzip_file(zip_filepath, extract_to):
+    if not os.listdir(extract_to):
+        #Folder Empty
+        try:
+            with zipfile.ZipFile(zip_filepath, 'r') as zip_ref:
+                zip_ref.extractall(extract_to)
+            print(f'Zip File Extracted to path {extract_to}')
+        except:
+            print(f'Zip File Could Not Be Extracted')
+    else:
+        #Folder not empty
+        print('Directory was not empty. Deleted Directory')
+        clean_dir(extract_to)
+        create_dir(extract_to)
+        try:
+            with zipfile.ZipFile(zip_filepath, 'r') as zip_ref:
+                zip_ref.extractall(extract_to)
+            print(f'Zip File Extracted to path {extract_to}')
+        except:
+            print(f'Zip File Could Not Be Extracted')
+
+def read_params(config_path:str) -> dict:
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
